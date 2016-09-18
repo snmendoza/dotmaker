158abcbda59e4356dd551131c030228d416269ba
el-minadero <kmendoza4@ucmerced.edu>
2016-09-17 17:46:07 -0700

    cleaned up GUI and prepared main for use in app

--- dotmaker/GUI.py
+++ dotmaker/GUI.py
@@ -2,53 +2,66 @@
 from tkinter import *
 from tkinter import filedialog
 
+class gui_object:
+    def __init__(self):
+# All other 'global variabels' are initialized in the constructor.
+        self.root = Tk()
+        self.from_Base_File = IntVar()
+        self.inpath  = StringVar()
+        self.outpath = StringVar()
+        self.__create_GUI()
+        self.options=Frame(self.root).grid(row=1, column=4)
 
-def browsein():
-    I = filedialog.askdirectory(parent=root, initialdir='~/')
-    inpath.set(I)
-def browseout():
-    O = filedialog.askdirectory(parent=root, initialdir='~/')
-    outpath.set(O)
+# I'm assuming this is to pull in files from a directory
+    def __browsein(self):
+        I = filedialog.askdirectory(parent=self.root, initialdir='~/')
+        self.inpath.set(I)
 
-root = Tk()
-var = IntVar()
-inpath  = StringVar()
-outpath = StringVar()
+# I'm assuming this is to save files in a directory
+    def __browseout(self):
+        O = filedialog.askdirectory(parent=self.root, initialdir='~/')
+        self.outpath.set(O)
 
+# Creates the GUI we all know and love
+    def __create_GUI(self):
+        self.__create_labels()
+        self.__create_buttons()
+        self.__create_graphic()
 
+        #what do these two do vvv ??
+        self.root.after(10, root.update_idletasks())
+        self.root.mainloop()
 
-Label(root, text="Dot Maker", font=("Helvetica", 18)).grid(row=0, columnspan=4)
+    def __create_graphic(self):
+        # Hm, seems like we need to self-reference a dummy eye image in our
+        # resources folder. right now it's relative to your computer's filesystem
+        image = PhotoImage(file="/Users/sean/Documents/Contact GUI/dot.gif")
+        dotimage = Label(root, image=image, height=210, width=240)
+        dotimage.image = image
+        dotimage.grid(row=1,column=0,columnspan=2, padx=(10,5))
+        image2 = PhotoImage(file="/Users/sean/Documents/Contact GUI/dot.gif")
+        dotimage2 = Label(root, image=image, height=210, width=240)
+        dotimage2.image = image
+        dotimage2.grid(row=1,column=2,columnspan=2, padx=(5,10))
 
-options=Frame(root)
-options.grid(row=1, column=4)
-Label(options, text="  Image Created", font=("Helvetica", 14)).grid(sticky='w')
-Label(options, text="", font=("Helvetica", 6)).grid(row=1)
-Radiobutton(options, text='From base file', variable = var, value=1).grid(row=2, sticky='w')
-Radiobutton(options, text='Generated', variable = var, value=2).grid(row=3, sticky='w')
-Label(options, text="", font=("Helvetica", 8)).grid(row=4)
-Label(options, text="  Options", font=("Helvetica", 14)).grid(row=5, sticky='w')
+    def __create_buttons(self):
+        Radiobutton(self.options, text='From base file', variable = self.from_Base_File, value=1).grid(row=2, sticky='w')
+        Radiobutton(self.options, text='Generated', variable = self.from_Base_File, value=2).grid(row=3, sticky='w')
+        b = Button(self.root, text=" Browse ", command=self.__browsein())
+        b.grid(row=3, column=3,sticky='e', padx=20)
+        b1 = Button(self.root, text=" Browse ", command=self.__browseout())
+        b1.grid(row=4, column=3, sticky='e', padx=20)
+        Button(self.root, text=" Generate ", padx=5, pady=5).grid(row=3, column=4, rowspan=2)
 
-image = PhotoImage(file="/Users/sean/Documents/Contact GUI/dot.gif")
-dotimage = Label(root, image=image, height=210, width=240)
-dotimage.image = image
-dotimage.grid(row=1,column=0,columnspan=2, padx=(10,5))
-
-image2 = PhotoImage(file="/Users/sean/Documents/Contact GUI/dot.gif")
-dotimage2 = Label(root, image=image, height=210, width=240)
-dotimage2.image = image
-dotimage2.grid(row=1,column=2,columnspan=2, padx=(5,10))
-
-Label(root, text="Input File Path:").grid(row=3, column=0, sticky='W')
-Label(root, text="Output File Path:").grid(row=4, column=0, sticky='W')
-
-Label(root, textvariable=inpath).grid(row=3, column=1, sticky='W')
-Label(root, textvariable=outpath).grid(row=4, column=1, sticky='W')
-
-b = Button(root, text=" Browse ", command=browsein)
-b.grid(row=3, column=3,sticky='e', padx=20)
-b1 = Button(root, text=" Browse ", command=browseout)
-b1.grid(row=4, column=3, sticky='e', padx=20)
-Button(root, text=" Generate ", padx=5, pady=5).grid(row=3, column=4, rowspan=2)
-
-root.after(10, root.update_idletasks())
-root.mainloop()
+    def __create_labels(self):
+        #Main Labels
+        Label(self.root, text="Dot Maker", font=("Helvetica", 18)).grid(row=0, columnspan=4)
+        Label(self.options, text="  Image Created", font=("Helvetica", 14)).grid(sticky='w')
+        Label(self.options, text="", font=("Helvetica", 6)).grid(row=1)
+        #Sub Labels
+        Label(self.options, text="", font=("Helvetica", 8)).grid(row=4)
+        Label(self.options, text="  Options", font=("Helvetica", 14)).grid(row=5, sticky='w')
+        Label(self.root, text="Input File Path:").grid(row=3, column=0, sticky='W')
+        Label(self.root, text="Output File Path:").grid(row=4, column=0, sticky='W')
+        Label(self.root, textvariable=self.inpath).grid(row=3, column=1, sticky='W')
+        Label(self.root, textvariable=self.outpath).grid(row=4, column=1, sticky='W')
--- dotmaker/main.py
+++ dotmaker/main.py
@@ -1,57 +1,8 @@
 import sys
 from png_interface import png_maker
+from GUI import gui_object
 
 def main():
-    printTitle()
-    inputLoop()
-
-def inputLoop():
-    input_var = input("")
-    parseInput(input_var)
-
-def parseInput(input_var):
-    if input_var   == "ls":
-        lsOptions()
-        inputLoop()
-    elif input_var == "1":
-        startOperations(1)
-    elif input_var == "2":
-        startOperations(2)
-    elif input_var == "3":
-        startOperations(3)
-    elif input_var == "exit":
-        sys.exit()
-    else:
-        print("Sorry, that is not a valid option. Enter \'ls\' for a list of options")
-        inputLoop()
-
-def startOperations(options):
-    png_setup = png_maker(options)
-    if png_setup.validFile==True or png_setup.validPath==True:
-        if options == 3:
-            png_setup.multipng()
-        else:
-            png_setup.createpng(False)
-        print("File Created. Back to Main Menu")
-    else:
-        print("Select your option again, or press 'ls' for other options")
-    inputLoop()
-
-def lsOptions():
-    print("""\n*********************************************************
-* To add dots to an input png image, type \t\'1\'\t*
-* To make a dotted png image, type \t\t\'2\'\t*
-* To make a varied dotted palette, type \t\'3\'\t*
-* To exit Dot maker, type \t\t\t\'exit\'\t*
-*********************************************************\n""")
-
-def printTitle():
-    print("""\n*********************************************************
-*\t\t Welcome to Dot Maker\t\t\t*
-*\t\t\t\t\t\t\t*
-*\t\tMade by Kevin Mendoza\t\t\t*
-*********************************************************
-*\t type \'ls\' for a list of commands\t\t*
-*********************************************************\n""")
-
+    gui = gui_object()
+    
 main()
